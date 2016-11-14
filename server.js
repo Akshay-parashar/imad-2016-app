ed2542476f630c82bc8f9d38b83f504176c9a789
Akshay Parashar <akshay27041995@gmail.com>
2016-11-14 17:18:58 +0530

    Improved UI

--- server.js
+++ server.js
@@ -1,12 +1,51 @@
+// Libraries used
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var Pool = require('pg').Pool;
+
+var congif = {
+  user: 'postgres',
+  database: 'postgres',
+  host: 'localhost',
+  port: '5432',
+  password: process.env.DB_PASS
+};
 
 var app = express();
 app.use(morgan('combined'));
+/*var phpExpress = require('php-express')({
+ 
+  // assumes php is in your PATH
+  binPath: 'php'
+});
+
+// set view engine to php-express
+app.set('views', './views');
+app.engine('php', phpExpress.engine);
+app.set('view engine', 'php');
+ 
+// routing all .php file to php-express
+app.all(/.+\.php$/, phpExpress.router);
+var mysql      = require('mysql');
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : process.env.DB_PASS,
+  database : 'test'
+});*/
+
+
+
+//-------------------------
+
 // Registering Static Files
 app.use(express.static('img'));
 app.use(express.static('css'));
+
+//---------------------------
+
+//General Files
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
@@ -28,6 +67,45 @@ app.get('/article-three', function(req,res) {
 app.get('/web', function(req,res) {
 	 res.sendFile(path.join(__dirname, 'ui', 'web.html'));
 });
+
+
+ //make a connection pool
+ var pool = new Pool(congif);
+
+app.get('/test-db', function(req,res){
+   
+    //make a select query 
+    pool.query('SELECT * FROM test', function(err,result){
+      if (err) {
+          res.status(500).send(err.toString());
+      }
+      else{
+        res.send(JSON.stringify(result.rows));
+      }
+    });
+
+    //close connection
+});
+
+
+app.get('/article_one',function(req,res){
+    pool.query("Select * from article where title = 'Article-Two' ",function(err,result){
+        if(err){
+          res.status(500).send(err.toString());
+          console.log("Error situation in db most probab in query")
+        }
+        else {
+          if(result.rows.length == 0){
+            res.send(404).send("Article Not Found");
+          }
+          else{
+            res.send(JSON.stringify(result.rows));
+          }
+      }
+    });
+});
+
+//------------------------------------
 
 //Responses for Stylesheets 
 
@@ -74,6 +152,9 @@ app.get('/ui/scripts/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui/scripts', 'main.js'));
 });
 
+app.get('/ui/scripts/jquery.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui/scripts', 'jquery.js'));
+});
 //-----------------------------
 
 
--- ui/scripts/main.js
+++ ui/scripts/main.js
@@ -1,1 +1,23 @@
-console.log('Loaded!');
+//var blog_container = document.getElementById('blog_pos_con');
+
+$(document).ready(function () {
+	$("#bg_date").click(function(){
+		$(this).hide();
+		console.log("WOOOOOHOOOOO!!!!!")
+	});
+
+	var con = `<div class="post" > 
+                        <h3 class="post_heading">Article one</h3>
+                        <span class="post_date" id="bg_date">10-10-2016</span><span class="post_comments"># Comments</span>
+                        <p class="post_conent">This is my first blog post article. And yes i know it took me a long time to develop this layout bt yeah it was worth it.
+                        And the said part is,this is only the ui part without any jquery or stuff i still have to add the backend logic and connecting this to my database and changing the server js and main js files. </p>
+                        <a href="#">Comment</a>
+                        <hr>
+                    </div>`
+
+	$("#bg_date").click(function(){
+		console.log("Executed")
+		$("#blog_pos_con").append(con);
+	});
+
+});
